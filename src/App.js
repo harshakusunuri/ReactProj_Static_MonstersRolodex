32d1cd4479fc1050e28f952277d833afddec4f79
SriHarsha <harshakusunuri@gmail.com>
2021-05-20 21:39:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class='home'>
+          <span class='child'> VEG</span>
+          <span class='child'>NON_VEG</span>
+
+        </div>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
